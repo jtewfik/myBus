a4bb257af71749f24fc0fa0561aa6859974c5751
jtewfik <root@John-Tewfiks-MacBook.local>
2013-12-29 00:04:04 -0800

    Updating to use new config page

---
+++ build/appinfo.auto.c
@@ -0,0 +1,20 @@
+#include "pebble_app_info.h"
+#include "src/resource_ids.auto.h"
+
+const PebbleAppInfo __pbl_app_info __attribute__ ((section (".pbl_header"))) = {
+  .header = "PBLAPP",
+  .struct_version = { APP_INFO_CURRENT_STRUCT_VERSION_MAJOR, APP_INFO_CURRENT_STRUCT_VERSION_MINOR },
+  .sdk_version = { APP_INFO_CURRENT_SDK_VERSION_MAJOR, APP_INFO_CURRENT_SDK_VERSION_MINOR },
+  .app_version = { 1, 0 },
+  .load_size = 0xb6b6,
+  .offset = 0xb6b6b6b6,
+  .crc = 0xb6b6b6b6,
+  .name = "My Bus",
+  .company = "MakeAwesomeHappen",
+  .icon_resource_id = DEFAULT_MENU_ICON,
+  .sym_table_addr = 0xA7A7A7A7,
+  .flags = 0,
+  .num_reloc_entries = 0xdeadcafe,
+  .uuid = { 0x1F, 0x48, 0x9A, 0x04, 0xC3, 0x24, 0x4B, 0x45, 0xA0, 0xB0, 0x84, 0xB7, 0xBF, 0x53, 0x5B, 0x3A },
+  .virtual_size = 0xb6b6
+};
