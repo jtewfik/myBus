7f582f1e8c518baf17296deef8573e25e6503fa0
jtewfik <root@John-Tewfiks-MacBook.local>
2013-12-29 11:50:37 -0800

    updating to gh pages URL

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,7 +1,13 @@
 var stop_num = 50182;
 var route_num = 003;
 
-function fetchStopTime(stop_num, route_num) {
+function fetchStopTime(stop_num_param, route_num_param) {
+	//update local vars
+	stop_num = stop_num_param;
+	route_num = route_num_param;
+
+	console.log("fetching stop time for  stop# " + stop_num + " and route " + route_num);
+
 	var response;
 	var req = new XMLHttpRequest();
 	req.open('GET', "http://api.translink.ca/RTTIAPI/V1/stops/" + stop_num + "/estimates?timeframe=120&apiKey=xrt9jWz1VzXJ7unF1tlv&routeNo=" + route_num + "&count=1", true);
@@ -47,4 +53,29 @@ Pebble.addEventListener("appmessage",
 
 							fetchStopTime(stop_num, route_num);
 							
+						});
+
+Pebble.addEventListener("showConfiguration",
+						function(e) {
+							console.log("Showing configuration");
+							Pebble.openURL('http://jtewfik.github.io/myBus/index.html');
+						});
+
+Pebble.addEventListener("webviewclosed",
+						function(e) {
+							console.log("Closing configuration");
+							//store the settings
+							if (e.response) {
+								var config = JSON.parse(e.response);
+								console.log("Configuration window returned: " + JSON.stringify(config));
+
+								//set the params and log them
+								console.log("Stop Number: " + config.stop_num);
+								console.log("Route Number: " + config.route_num);
+								localStorage.setItem("stop_num", config.stop_num);
+								localStorage.setItem("route_num", config.route_num);
+
+								fetchStopTime(config.stop_num, config.route_num);
+							}
+
 						});
